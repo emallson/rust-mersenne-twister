b8742042622fcf29a6295b9a6f793a573ff19e4a
J David Smith <emallson@atlanis.net>
2017-06-30 11:29:22 -0400

    Added `mersenne` function to initialize a MTRng easily

--- src/lib.rs
+++ src/lib.rs
@@ -10,3 +10,18 @@ pub use self::mt64::MTRng64 as MTRng;
 
 pub mod mt32;
 pub mod mt64;
+
+/// Generates a `MTRng` object by seeding with bytes from `OsRng`
+pub fn mersenne() -> MTRng {
+    use rand::{Rng, OsRng};
+    const BYTES: usize = 100;
+
+    let mut os = OsRng::new().unwrap();
+
+    let mut seed = [0u64; BYTES];
+    for i in 0..BYTES {
+        seed[i] = os.next_u64();
+    }
+
+    MTRng::new_array(&seed)
+}
